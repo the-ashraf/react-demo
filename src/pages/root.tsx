47071dca6f126e8015f2fbc89b88ae57debbaa40
Ashraf Ezzat <hot4mix@gmail.com>
2023-10-31 08:18:23 +0200

    documant lang

--- src/pages/root.tsx
+++ src/pages/root.tsx
@@ -5,6 +5,7 @@ export default function Root() {
     const [ t, i18n ] = useTranslation();
     const isArabic = i18n.language === 'ar';
     document.body.dir = i18n.dir();
+    document.html.lang = i18n.language;
     return (
         <>
             <div className="grid min-h-screen grid-rows-[auto_1fr_auto] font-display text-secondary-500 antialiased relative bg-gray-50">
@@ -24,7 +25,7 @@ export default function Root() {
                                         onClick={() => { i18n.changeLanguage(isArabic ? 'en' : 'ar') }}
                                         className="px-3 py-1 bg-white text-emerald-900 border border-emerald-900 rounded-md capitalize hover:bg-emerald-100 duration-150"
                                     >
-                                        { isArabic ? 'ar' : 'en'}
+                                        { isArabic ? 'ع' : 'en'}
                                     </button>
                                 </li>
                             </ul>
